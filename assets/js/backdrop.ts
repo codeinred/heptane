f967689a44dd72ecd6db3be0fed61437ffc4e121
Alecto Irene Perez <perez.cs@pm.me>
2021-07-26 14:29:36 -0600

    Experimented with math backdrop

---
+++ assets/js/backdrop.ts
@@ -0,0 +1,50 @@
+let body = <HTMLBodyElement>document.getElementsByTagName('body')[0];
+
+body.onload = fill_backdrop;
+
+function fixCanvasDPI(canvas: HTMLCanvasElement, w, h) {
+    var context = canvas.getContext("2d"),
+        dpr: number = window.devicePixelRatio || 1,
+        bsr: number = context.webkitBackingStorePixelRatio ||
+            context.mozBackingStorePixelRatio ||
+            context.msBackingStorePixelRatio ||
+            context.oBackingStorePixelRatio ||
+            context.backingStorePixelRatio || 1;
+
+    var ratio = dpr / bsr;
+    canvas.width = w * ratio;
+    canvas.height = h * ratio;
+    canvas.style.width = w + "px";
+    canvas.style.height = h + "px";
+    canvas.getContext("2d").setTransform(ratio, 0, 0, ratio, 0, 0);
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+let symbols = "∀∁∂∃∄∅∆∇∈∉∊∋∌∍∎∏∐∑−∓∔∕∖∗∘∙√∛∜∝∞∟∠∡∢∣∤∥∦∧∨∩∪∫∬∭∮∯∰∱∲∳∴∵∶∷∸∹∺∻∼∽∾∿≀≁≂≃≄≅≆≇≈≉≊≋≌≍≎≏≐≑≒≓≔≕≖≗≘≙≚≛≜≝≞≟≠≡≢≣≤≥≦≧≨≩≪≫≬≭≮≯≰≱≲≳≴≵≶≷≸≹≺≻≼≽≾≿⊀⊁⊂⊃⊄⊅⊆⊇⊈⊉⊊⊋⊌⊍⊎⊏⊐⊑⊒⊓⊔⊕⊖⊗⊘⊙⊚⊛⊜⊝⊞⊟⊠⊡⊢⊣⊤⊥⊦⊧⊨⊩⊪⊫⊬⊭⊮⊯⊰⊱⊲⊳⊴⊵⊶⊷⊸⊹⊺⊻⊼⊽⊾⊿⋀⋁⋂⋃⋄⋅⋆⋇⋈⋉⋊⋋⋌⋍⋎⋏⋐⋑⋒⋓⋔⋕⋖⋗⋘⋙⋚⋛⋜⋝⋞⋟⋠⋡⋢⋣⋤⋥⋦⋧⋨⋩⋪⋫⋬⋭⋮⋯⋰⋱⋲⋳⋴⋵⋶⋷⋸⋹⋺⋻⋼⋽⋾⋿"
+
+function fill_backdrop() {
+    let canvas = <HTMLCanvasElement>document.getElementById('backdrop');
+    fixCanvasDPI(canvas, body.clientWidth, body.clientHeight);
+    let context = canvas.getContext('2d');
+    let width = canvas.width;
+    let height = canvas.height;
+
+    context.font = '20px serif'
+    context.fillStyle = '#00000030';
+    let radius = 64;
+    let dx = radius;
+    let dy = radius * Math.sqrt(3) / 2.0;
+    let shift = false;
+    let count = symbols.length;
+    for (var y = 0; y < height; y += dy) {
+        for (var x = shift ? radius / 2 : 0; x < width; x += dx) {
+            context.fillText(symbols[getRandomInt(0, count)], x, y);
+        }
+        shift = !shift;
+    }
+}
