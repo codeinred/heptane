f4b4fd8cb3961a905570cfaac26f92f7c051c0d2
Alecto Irene Perez <perez.cs@pm.me>
2021-07-29 17:42:12 -0600

    Fixed backdrop (used svg background)

--- assets/js/backdrop.ts
+++ assets/js/backdrop.ts
@@ -27,28 +27,114 @@ function getRandomInt(min, max) {
 
 let symbols = "∀∁∂∃∄∅∆∇∈∉∊∋∌∍∎∏∐∑−∓∔∕∖∗∘∙√∛∜∝∞∟∠∡∢∣∤∥∦∧∨∩∪∫∬∭∮∯∰∱∲∳∴∵∶∷∸∹∺∻∼∽∾∿≀≁≂≃≄≅≆≇≈≉≊≋≌≍≎≏≐≑≒≓≔≕≖≗≘≙≚≛≜≝≞≟≠≡≢≣≤≥≦≧≨≩≪≫≬≭≮≯≰≱≲≳≴≵≶≷≸≹≺≻≼≽≾≿⊀⊁⊂⊃⊄⊅⊆⊇⊈⊉⊊⊋⊌⊍⊎⊏⊐⊑⊒⊓⊔⊕⊖⊗⊘⊙⊚⊛⊜⊝⊞⊟⊠⊡⊢⊣⊤⊥⊦⊧⊨⊩⊪⊫⊬⊭⊮⊯⊰⊱⊲⊳⊴⊵⊶⊷⊸⊹⊺⊻⊼⊽⊾⊿⋀⋁⋂⋃⋄⋅⋆⋇⋈⋉⋊⋋⋌⋍⋎⋏⋐⋑⋒⋓⋔⋕⋖⋗⋘⋙⋚⋛⋜⋝⋞⋟⋠⋡⋢⋣⋤⋥⋦⋧⋨⋩⋪⋫⋬⋭⋮⋯⋰⋱⋲⋳⋴⋵⋶⋷⋸⋹⋺⋻⋼⋽⋾⋿"
 
-function fill_backdrop() {
-    let canvas = <HTMLCanvasElement>document.getElementById('backdrop');
-    fixCanvasDPI(canvas, body.clientWidth, body.clientHeight + 16 * 7);
-    let context = canvas.getContext('2d');
-    let width = canvas.width;
-    let height = canvas.height;
+function svgElem(name: string, properties: any = null, innerHTML: string = null) {
+    const elem = document.createElementNS('http://www.w3.org/2000/svg', name);
+    if (properties != null) {
+        for (var prop in Object.keys(properties)) {
+            elem.setAttribute(prop, `${properties[prop]}`);
+        }
+    }
+    if (innerHTML != null) {
+        elem.innerHTML = innerHTML;
+    }
+    return elem;
+}
+function getTextElem(text: string, x: number, y: number) {
+    const elem = svgElem('text');
+    elem.innerHTML = text;
+    elem.setAttribute('x', `${x}`);
+    elem.setAttribute('y', `${y}`);
+    return elem;
+}
+function getSVGBackgroundStyle(fontSize : number) {
+    const style = svgElem('style');
+    style.innerHTML = `
+    text {
+        font: bold ${fontSize}px serif;
+        fill: gold;
+        text-align: center;
+        text-baseline: middle;
+    }
+    `;
+    return style;
+}
+function getSVGBlurElement() {
+    const filter = svgElem('filter');
+    filter.setAttribute('id', 'blur');
 
-    context.font = '16px serif'
-    context.fillStyle = '#00000030';
-    context.textAlign = 'center';
-    context.textBaseline = 'middle';
-    let radius = 32;
-    let dx = radius;
-    let dy = radius * Math.sqrt(3) / 2.0;
-    let shift = false;
-    let count = symbols.length;
-    for (var y = radius / 2; y < height + dy; y += dy) {
-        for (var x = shift ? radius / 2 : 0; x < width; x += dx) {
+    const blur = svgElem('feGaussianBlur');
+    blur.setAttribute('in', 'SourceGraphic');
+    blur.setAttribute('stdDeviation', '5');
+    filter.appendChild(blur);
+    return filter;
+}
+function getSVGContents(id, fontSize, radius, width, height) {
+    const dx = radius;
+    const dy = radius * Math.sqrt(3) / 2.0;
+    const count = symbols.length;
+
+    const content = svgElem('symbol');
+    content.setAttribute('id', id);
+    content.setAttribute('width', `${width}`)
+    content.setAttribute('height', `${height}`)
+    content.setAttribute('viewBox', `0 0 ${width} ${height}`)
+
+    var shift = false;
+    for (var y = (radius + fontSize) / 2; y < height + dy; y += dy) {
+        for (var x = fontSize / 2 + (shift ? radius / 2 : 0); x < width; x += dx) {
             if(Math.random() < 0.5)
                 continue;
-            context.fillText(symbols[getRandomInt(0, count)], x, y);
-        }
+            const symbol = symbols[getRandomInt(0, count)];
+            content.appendChild(getTextElem(symbol, x, y));
+        }``
         shift = !shift;
     }
+    return content;
+}
+function getSVGBackground() {
+    const radius = 32;
+    const fontSize = 16;
+    const background = svgElem('svg');
+    const dx = radius;
+    const dy = radius * Math.sqrt(3) / 2.0;
+    const width = dx * 20;
+    const height = dy * 20;
+
+
+    background.setAttribute('width', `${width}`)
+    background.setAttribute('height', `${height}`)
+    background.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
+    background.setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink')
+
+    background.appendChild(getSVGContents('math', fontSize, radius, width, height));
+
+    background.appendChild(getSVGBackgroundStyle(fontSize));
+    background.appendChild(getSVGBlurElement());
+
+    const backContent = svgElem('use');
+    backContent.setAttribute('xlink:href', '#math');
+    backContent.setAttribute('x', '0');
+    backContent.setAttribute('y', '0');
+    backContent.setAttribute('filter', 'url(#blur)');
+    background.appendChild(backContent);
+
+    const mainContent = svgElem('use');
+    mainContent.setAttribute('xlink:href', '#math');
+    mainContent.setAttribute('x', '0');
+    mainContent.setAttribute('y', '0');
+    background.appendChild(mainContent);
+
+    return background;
+}
+
+function utf8_to_b64( str ) {
+    return window.btoa(unescape(encodeURIComponent( str )));
+}
+
+function fill_backdrop() {
+
+    const backdrop = document.getElementById('backdrop');
+    const svgBackgrond = getSVGBackground();
+    var mySVG64 = utf8_to_b64(svgBackgrond.outerHTML);
+    document.getElementById('backdrop').style.backgroundImage = `url('data:image/svg+xml;base64,${mySVG64}')`;
 }
